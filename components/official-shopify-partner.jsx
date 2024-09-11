5d075c2f8272a1502643e05db8c5c7916d321cc2
anaswhitefort <anasmuhammedinfo@gmail.com>
2024-09-12 03:25:28 +0400

    test: update changes

--- components/official-shopify-partner.jsx
+++ components/official-shopify-partner.jsx
@@ -4,78 +4,147 @@ import React from 'react';
 
 export function OfficialShopifyPartner() {
     return (
-        <div class="font-sans">
-      <div class="grid lg:grid-cols-2 items-center lg:gap-y-6 bg-black">
-        <div class="max-lg:order-1 max-lg:text-left sm:p-12 p-4">
-        <h2
-  className="text-white lg:text-5xl text-3xl font-bold lg:!leading-[56px]"
-  style={{
-    backgroundImage: 'linear-gradient(to bottom, #fff, #fff, rgba(255, 255, 255, 0.6))',
-    WebkitBackgroundClip: 'text',
-    backgroundClip: 'text',  // Fallback for non-webkit browsers
-    color: 'transparent'
-  }}
->
-  We're Official Shopify Partner in Abu Dhabi
-</h2>
-          <p class="text-white mt-6 text-base leading-relaxed">As an official Shopify Partner in Abu Dhabi, we specialize in comprehensive store development and digital marketing support. Since 2020, we've been dedicated to helping businesses succeed online with expert Shopify solutions.</p>
-          <button type='button'
-            class="bg-transparent border-2 border-white mt-12 transition-all text-white font-bold text-sm rounded-[10px] px-6 py-2.5">Read More</button>
-        </div>
+        <div className="font-sans">
+            <div className="grid lg:grid-cols-2 items-center lg:gap-y-6 bg-black">
+                <div className="max-lg:order-1 max-lg:text-left sm:p-12 p-4">
+                    <h2
+                        className="text-white lg:text-5xl text-3xl font-bold lg:!leading-[56px]"
+                        style={{
+                            backgroundImage: 'linear-gradient(to bottom, #fff, #fff, rgba(255, 255, 255, 0.6))',
+                            WebkitBackgroundClip: 'text',
+                            backgroundClip: 'text', // Fallback for non-webkit browsers
+                            color: 'transparent'
+                        }}
+                    >
+                        We are Official Shopify Partner in Abu Dhabi
+                    </h2>
+                    <p className="text-white mt-6 text-base leading-relaxed">
+                        As an official Shopify Partner in Abu Dhabi, we specialize in comprehensive store development and digital marketing support. Since 2020, we have been dedicated to helping businesses succeed online with expert Shopify solutions.
+                    </p>
+                    <button
+                        type='button'
+                        className="bg-transparent border-2 border-white mt-12 transition-all text-white font-bold text-sm rounded-[10px] px-6 py-2.5"
+                    >
+                        Read More
+                    </button>
+                </div>
 
-        <div class="lg:h-[345px] flex items-center "
+                <div className="lg:h-[345px] flex items-center">
+                    <img
+                        src="/images/Official_shopify_partner_abu_dhabi.png"
+                        className="w-full h-full object-cover border-white rounded-[15px]"
+                        alt="Dining Experience"
+                    />
+                </div>
+            </div>
+
+            <div className="grid lg:grid-cols-4 md:grid-cols-2 gap-6 px-4 my-12">
+                <div
+                    className="bg-black p-6 rounded-[15px] border border-white"
+                    style={{ boxShadow: '4px 6px 0px 4px #35cbee' }}
                 >
-          <img src="/images/Official_shopify_partner_abu_dhabi.png" class="w-full h-full object-cover border-white rounded-[15px]" alt="Dining Experience" />
-        
-        </div>
-      </div>
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="#fff"
+                        className="w-10 h-10 mb-4 inline-block p-2 rounded-md"
+                        style={{ backgroundColor: '#35cbee' }}
+                        viewBox="0 0 32 32"
+                    >
+                        <path
+                            d="M28.068 12h-.128a.934.934 0 0 1-.864-.6.924.924 0 0 1 .2-1.01l.091-.091a2.938 2.938 0 0 0 0-4.147l-1.511-1.51a2.935 2.935 0 0 0-4.146 0l-.091.091A.956.956 0 0 1 20 4.061v-.129A2.935 2.935 0 0 0 17.068 1h-2.136A2.935 2.935 0 0 0 12 3.932v.129a.956.956 0 0 1-1.614.668l-.086-.091a2.935 2.935 0 0 0-4.146 0l-1.516 1.51a2.938 2.938 0 0 0 0 4.147l.091.091a.935.935 0 0 1 .185 1.035.924.924 0 0 1-.854.579h-.128A2.935 2.935 0 0 0 1 14.932v2.136A2.935 2.935 0 0 0 3.932 20h.128a.934.934 0 0 1 .864.6.924.924 0 0 1-.2 1.01l-.091.091a2.938 2.938 0 0 0 0 4.147l1.51 1.509a2.934 2.934 0 0 0 4.147 0l.091-.091a.936.936 0 0 1 1.035-.185.922.922 0 0 1 .579.853v.129A2.935 2.935 0 0 0 14.932 31h2.136A2.935 2.935 0 0 0 20 28.068v-.129a.956.956 0 0 1 1.614-.668l.091.091a2.935 2.935 0 0 0 4.146 0l1.511-1.509a2.938 2.938 0 0 0 0-4.147l-.091-.091a.935.935 0 0 1-.185-1.035.924.924 0 0 1 .854-.58h.128A2.935 2.935 0 0 0 31 17.068v-2.136A2.935 2.935 0 0 0 28.068 12ZM29 17.068a.933.933 0 0 1-.932.932h-.128a2.956 2.956 0 0 0-2.083 5.028l.09.091a.934.934 0 0 1 0 1.319l-1.511 1.509a.932.932 0 0 1-1.318 0l-.09-.091A2.957 2.957 0 0 0 18 27.939v.129a.933.933 0 0 1-.932.932h-2.136a.933.933 0 0 1-.932-.932v-.129a2.951 2.951 0 0 0-5.028-2.082l-.091.091a.934.934 0 0 1-1.318 0l-1.51-1.509a.934.934 0 0 1 0-1.319l.091-.091A2.956 2.956 0 0 0 4.06 18h-.128A.933.933 0 0 1 3 17.068v-2.136A.933.933 0 0 1 3.932 14h.128a2.956 2.956 0 0 0 2.083-5.028l-.09-.091a.933.933 0 0 1 0-1.318l1.51-1.511a.932.932 0 0 1 1.318 0l.09.091A2.957 2.957 0 0 0 14 4.061v-.129A.933.933 0 0 1 14.932 3h2.136a.933.933 0 0 1 .932.932v.129a2.956 2.956 0 0 0 5.028 2.082l.091-.091a.932.932 0 0 1 1.318 0l1.51 1.511a.933.933 0 0 1 0 1.318l-.091.091A2.956 2.956 0 0 0 27.94 14h.128a.933.933 0 0 1 .932.932Z"
+                            fillRule="evenodd"
+                            clipRule="evenodd"
+                        />
+                        <path
+                            d="M16 9a7 7 0 1 0 7 7 7.008 7.008 0 0 0-7-7Zm0 12a5 5 0 1 1 5-5 5.006 5.006 0 0 1-5 5Z"
+                            fillRule="evenodd"
+                            clipRule="evenodd"
+                        />
+                    </svg>
+                    <h3 className="text-lg font-bold mb-2 text-white">Custom Store Setup & Customization</h3>
+                    <p className="text-sm text-white">
+                        Experienced Shopify developer in Abu Dhabi offering tailored store setup, theme customization, and seamless user experience for your brand.
+                    </p>
+                </div>
 
-      <div class="grid lg:grid-cols-4 md:grid-cols-2 gap-6 px-4 my-12">
-      <div className="bg-black p-6 rounded-[15px] border border-white" 
-  style={{ boxShadow: '4px 6px 0px 4px #35cbee' }}>
-          <svg xmlns="http://www.w3.org/2000/svg" fill="#fff" className="w-10 h-10 mb-4 inline-block p-2 rounded-md" style={{ backgroundColor: '#35cbee' }} viewBox="0 0 32 32">
-            <path d="M28.068 12h-.128a.934.934 0 0 1-.864-.6.924.924 0 0 1 .2-1.01l.091-.091a2.938 2.938 0 0 0 0-4.147l-1.511-1.51a2.935 2.935 0 0 0-4.146 0l-.091.091A.956.956 0 0 1 20 4.061v-.129A2.935 2.935 0 0 0 17.068 1h-2.136A2.935 2.935 0 0 0 12 3.932v.129a.956.956 0 0 1-1.614.668l-.086-.091a2.935 2.935 0 0 0-4.146 0l-1.516 1.51a2.938 2.938 0 0 0 0 4.147l.091.091a.935.935 0 0 1 .185 1.035.924.924 0 0 1-.854.579h-.128A2.935 2.935 0 0 0 1 14.932v2.136A2.935 2.935 0 0 0 3.932 20h.128a.934.934 0 0 1 .864.6.924.924 0 0 1-.2 1.01l-.091.091a2.938 2.938 0 0 0 0 4.147l1.51 1.509a2.934 2.934 0 0 0 4.147 0l.091-.091a.936.936 0 0 1 1.035-.185.922.922 0 0 1 .579.853v.129A2.935 2.935 0 0 0 14.932 31h2.136A2.935 2.935 0 0 0 20 28.068v-.129a.956.956 0 0 1 1.614-.668l.091.091a2.935 2.935 0 0 0 4.146 0l1.511-1.509a2.938 2.938 0 0 0 0-4.147l-.091-.091a.935.935 0 0 1-.185-1.035.924.924 0 0 1 .854-.58h.128A2.935 2.935 0 0 0 31 17.068v-2.136A2.935 2.935 0 0 0 28.068 12ZM29 17.068a.933.933 0 0 1-.932.932h-.128a2.956 2.956 0 0 0-2.083 5.028l.09.091a.934.934 0 0 1 0 1.319l-1.511 1.509a.932.932 0 0 1-1.318 0l-.09-.091A2.957 2.957 0 0 0 18 27.939v.129a.933.933 0 0 1-.932.932h-2.136a.933.933 0 0 1-.932-.932v-.129a2.951 2.951 0 0 0-5.028-2.082l-.091.091a.934.934 0 0 1-1.318 0l-1.51-1.509a.934.934 0 0 1 0-1.319l.091-.091A2.956 2.956 0 0 0 4.06 18h-.128A.933.933 0 0 1 3 17.068v-2.136A.933.933 0 0 1 3.932 14h.128a2.956 2.956 0 0 0 2.083-5.028l-.09-.091a.933.933 0 0 1 0-1.318l1.51-1.511a.932.932 0 0 1 1.318 0l.09.091A2.957 2.957 0 0 0 14 4.061v-.129A.933.933 0 0 1 14.932 3h2.136a.933.933 0 0 1 .932.932v.129a2.956 2.956 0 0 0 5.028 2.082l.091-.091a.932.932 0 0 1 1.318 0l1.51 1.511a.933.933 0 0 1 0 1.318l-.091.091A2.956 2.956 0 0 0 27.94 14h.128a.933.933 0 0 1 .932.932Z" data-original="#000000" />
-            <path d="M16 9a7 7 0 1 0 7 7 7.008 7.008 0 0 0-7-7Zm0 12a5 5 0 1 1 5-5 5.006 5.006 0 0 1-5 5Z" data-original="#000000" />
-          </svg>
-          <h3 class="text-lg font-bold mb-2 text-white">Custom Store Setup & Customization</h3>
-          <p class="text-sm text-white">Experienced Shopify developer in Abu Dhabi offering tailored store setup, theme customization, and seamless user experience for your brand.</p>
-        </div>
-        <div className="bg-black p-6 rounded-[15px] border border-white" 
-  style={{ boxShadow: '4px 6px 0px 4px #35cbee' }}>
-          <svg xmlns="http://www.w3.org/2000/svg" fill="#fff" className="w-10 h-10 mb-4 inline-block p-2 rounded-md" style={{ backgroundColor: '#35cbee' }}  viewBox="0 0 1024 1024"><path d="M896 554.666667v170.666666a85.333333 85.333333 0 0 1-85.333333 85.333334v42.666666a128 128 0 0 1-128 128h-128a42.666667 42.666667 0 0 1-42.666667-42.666666v-21.333334a21.333333 21.333333 0 0 1 21.333333-21.333333H682.666667a42.666667 42.666667 0 0 0 42.666666-42.666667v-42.666666a42.666667 42.666667 0 0 1-42.666666-42.666667v-256a42.666667 42.666667 0 0 1 42.666666-42.666667V341.333333A213.333333 213.333333 0 0 0 298.666667 341.333333v128a42.666667 42.666667 0 0 1 42.666666 42.666667v256a42.666667 42.666667 0 0 1-42.666666 42.666667H213.333333a85.333333 85.333333 0 0 1-85.333333-85.333334v-170.666666a85.333333 85.333333 0 0 1 85.333333-85.333334V341.333333a298.666667 298.666667 0 0 1 597.333334 0v128a85.333333 85.333333 0 0 1 85.333333 85.333334z"/></svg>
-          <h3 class="text-lg font-bold mb-2 text-white">Theme Support</h3>
-          <p class="text-sm text-white">We offer expert theme support and customization to ensure your store perfectly aligns with your brand identity. From modifying existing themes to creating custom layouts, we enhance both functionality and aesthetics</p>
-        </div>
+                <div
+                    className="bg-black p-6 rounded-[15px] border border-white"
+                    style={{ boxShadow: '4px 6px 0px 4px #35cbee' }}
+                >
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="#fff"
+                        className="w-10 h-10 mb-4 inline-block p-2 rounded-md"
+                        style={{ backgroundColor: '#35cbee' }}
+                        viewBox="0 0 1024 1024"
+                    >
+                        <path d="M896 554.666667v170.666666a85.333333 85.333333 0 0 1-85.333333 85.333334v42.666666a128 128 0 0 1-128 128h-128a42.666667 42.666667 0 0 1-42.666667-42.666666v-21.333334a21.333333 21.333333 0 0 1 21.333333-21.333333H682.666667a42.666667 42.666667 0 0 0 42.666666-42.666667v-42.666666a42.666667 42.666667 0 0 1-42.666666-42.666667v-256a42.666667 42.666667 0 0 1 42.666666-42.666667V341.333333A213.333333 213.333333 0 0 0 298.666667 341.333333v128a42.666667 42.666667 0 0 1 42.666666 42.666667v256a42.666667 42.666667 0 0 1-42.666666 42.666667H213.333333a85.333333 85.333333 0 0 1-85.333333-85.333334v-170.666666a85.333333 85.333333 0 0 1 85.333333-85.333334V341.333333a298.666667 298.666667 0 0 1 597.333334 0v128a85.333333 85.333333 0 0 1 85.333333 85.333334z" />
+                    </svg>
+                    <h3 className="text-lg font-bold mb-2 text-white">Theme Support</h3>
+                    <p className="text-sm text-white">
+                        We offer expert theme support and customization to ensure your store perfectly aligns with your brand identity. From modifying existing themes to creating custom layouts, we enhance both functionality and aesthetics.
+                    </p>
+                </div>
 
-      <div className="bg-black p-6 rounded-[15px] border border-white" 
-        style={{ boxShadow: '4px 6px 0px 4px #35cbee' }}>
-          <svg xmlns="http://www.w3.org/2000/svg" fill="#fff" className="w-10 h-10 mb-4 inline-block p-2 rounded-md" style={{ backgroundColor: '#35cbee' }}  viewBox="0 0 682.667 682.667">
-            <defs>
-              <clipPath id="a" clipPathUnits="userSpaceOnUse">
-                <path d="M0 512h512V0H0Z" data-original="#000000" />
-              </clipPath>
-            </defs>
-            <g fill="none" stroke="#fff" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" stroke-width="40" clip-path="url(#a)" transform="matrix(1.33 0 0 -1.33 0 682.667)">
-              <path d="M256 492 60 410.623v-98.925C60 183.674 137.469 68.38 256 20c118.53 48.38 196 163.674 196 291.698v98.925z" data-original="#000000" />
-              <path d="M178 271.894 233.894 216 334 316.105" data-original="#000000" />
-            </g>
-          </svg>
-          <h3 class="text-lg font-bold mb-2 text-white">Secure Lightning Fast Store</h3>
-          <p class="text-sm text-white">We offer a Secure and Lightning Fast Store that combines rapid delivery with robust protection. We prioritize your security while ensuring that you receive your orders swiftly and store efficiently.</p>
+                <div
+                    className="bg-black p-6 rounded-[15px] border border-white"
+                    style={{ boxShadow: '4px 6px 0px 4px #35cbee' }}
+                >
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="#fff"
+                        className="w-10 h-10 mb-4 inline-block p-2 rounded-md"
+                        style={{ backgroundColor: '#35cbee' }}
+                        viewBox="0 0 682.667 682.667"
+                    >
+                        <defs>
+                            <clipPath id="a" clipPathUnits="userSpaceOnUse">
+                                <path d="M0 512h512V0H0Z" />
+                            </clipPath>
+                        </defs>
+                        <g
+                            fill="none"
+                            stroke="#fff"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeMiterlimit="10"
+                            strokeWidth="40"
+                            clipPath="url(#a)"
+                            transform="matrix(1.33 0 0 -1.33 0 682.667)"
+                        >
+                            <path d="M256 492 60 410.623v-98.925C60 183.674 137.469 68.38 256 20c118.53 48.38 196 163.674 196 291.698v98.925z" />
+                            <path d="M178 271.894 233.894 216 334 316.105" />
+                        </g>
+                    </svg>
+                    <h3 className="text-lg font-bold mb-2 text-white">Secure Lightning Fast Store</h3>
+                    <p className="text-sm text-white">
+                        We offer a Secure and Lightning Fast Store that combines rapid delivery with robust protection. We prioritize your security while ensuring that you receive your orders swiftly and store efficiently.
+                    </p>
+                </div>
+
+                <div
+                    className="bg-black p-6 rounded-[15px] border border-white"
+                    style={{ boxShadow: '4px 6px 0px 4px #35cbee' }}
+                >
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="#fff"
+                        className="w-10 h-10 mb-4 inline-block p-2 rounded-md"
+                        style={{ backgroundColor: '#35cbee' }}
+                        viewBox="0 0 24 24"
+                    >
+                        <g fillRule="evenodd" clipRule="evenodd">
+                            <path d="M17.03 8.97a.75.75 0 0 1 0 1.06l-4.2 4.2a.75.75 0 0 1-1.154-.114l-1.093-1.639L8.03 15.03a.75.75 0 0 1-1.06-1.06l3.2-3.2a.75.75 0 0 1 1.154.114l1.093 1.639L15.97 8.97a.75.75 0 0 1 1.06 0z" />
+                            <path d="M13.75 9.5a.75.75 0 0 1 .75-.75h2a.75.75 0 0 1 .75.75v2a.75.75 0 0 1-1.5 0v-1.25H14.5a.75.75 0 0 1-.75-.75z" />
+                            <path d="M3.095 3.095C4.429 1.76 6.426 1.25 9 1.25h6c2.574 0 4.57.51 5.905 1.845C22.24 4.429 22.75 6.426 22.75 9v6c0 2.574-.51 4.57-1.845 5.905C19.571 22.24 17.574 22.75 15 22.75H9c-2.574 0-4.57-.51-5.905-1.845C1.76 19.571 1.25 17.574 1.25 15V9c0-2.574.51-4.57 1.845-5.905zm1.06 1.06C3.24 5.071 2.75 6.574 2.75 9v6c0 2.426.49 3.93 1.405 4.845.916.915 2.419 1.405 4.845 1.405h6c2.426 0 3.93-.49 4.845-1.405.915-.916 1.405-2.419 1.405-4.845V9c0-2.426-.49-3.93-1.405-4.845C18.929 3.24 17.426 2.75 15 2.75H9c-2.426 0-3.93.49-4.845 1.405z" />
+                        </g>
+                    </svg>
+                    <h3 className="text-lg font-bold mb-2 text-white">Marketing Support</h3>
+                    <p className="text-sm text-white">
+                        Comprehensive Shopify store marketing support designed to elevate your online presence and maximize your business potential. Our services include targeted strategies to drive traffic, optimize conversions, and increase sales.
+                    </p>
+                </div>
+            </div>
         </div>
-        <div className="bg-black p-6 rounded-[15px] border border-white" 
-  style={{ boxShadow: '4px 6px 0px 4px #35cbee' }}>
-          <svg xmlns="http://www.w3.org/2000/svg" fill="#fff" className="w-10 h-10 mb-4 inline-block p-2 rounded-md" style={{ backgroundColor: '#35cbee' }}  viewBox="0 0 24 24">
-            <g fill-rule="evenodd" clip-rule="evenodd">
-              <path d="M17.03 8.97a.75.75 0 0 1 0 1.06l-4.2 4.2a.75.75 0 0 1-1.154-.114l-1.093-1.639L8.03 15.03a.75.75 0 0 1-1.06-1.06l3.2-3.2a.75.75 0 0 1 1.154.114l1.093 1.639L15.97 8.97a.75.75 0 0 1 1.06 0z" data-original="#000000" />
-              <path d="M13.75 9.5a.75.75 0 0 1 .75-.75h2a.75.75 0 0 1 .75.75v2a.75.75 0 0 1-1.5 0v-1.25H14.5a.75.75 0 0 1-.75-.75z" data-original="#000000" />
-              <path d="M3.095 3.095C4.429 1.76 6.426 1.25 9 1.25h6c2.574 0 4.57.51 5.905 1.845C22.24 4.429 22.75 6.426 22.75 9v6c0 2.574-.51 4.57-1.845 5.905C19.571 22.24 17.574 22.75 15 22.75H9c-2.574 0-4.57-.51-5.905-1.845C1.76 19.571 1.25 17.574 1.25 15V9c0-2.574.51-4.57 1.845-5.905zm1.06 1.06C3.24 5.071 2.75 6.574 2.75 9v6c0 2.426.49 3.93 1.405 4.845.916.915 2.419 1.405 4.845 1.405h6c2.426 0 3.93-.49 4.845-1.405.915-.916 1.405-2.419 1.405-4.845V9c0-2.426-.49-3.93-1.405-4.845C18.929 3.24 17.426 2.75 15 2.75H9c-2.426 0-3.93.49-4.845 1.405z" data-original="#000000" />
-            </g>
-          </svg>
-          <h3 class="text-lg font-bold mb-2 text-white">Marketing Support</h3>
-          <p class="text-sm text-white">Comprehensive Shopify store marketing support designed to elevate your online presence and maximize your business potential. Our services include targeted strategies to drive traffic, optimize conversions, and increase sales.</p>
-       </div>
-      </div>
-    </div>
     );
 }
